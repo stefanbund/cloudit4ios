ab63e46634cb4441c3d54e6b1be9b0fe0ae9882e
Stefan Bund <sbund@nextacropolis.com>
2013-10-15 15:57:41 -0700

    Initial Commit

---
+++ cloudit4ios/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  cloudit4ios
+//
+//  Created by stefan bund on 10/15/13.
+//  Copyright (c) 2013 next acropolis, llc & stefan bund. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ cloudit4iosTests/cloudit4iosTests.m
@@ -0,0 +1,34 @@
+//
+//  cloudit4iosTests.m
+//  cloudit4iosTests
+//
+//  Created by stefan bund on 10/15/13.
+//  Copyright (c) 2013 next acropolis, llc & stefan bund. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface cloudit4iosTests : XCTestCase
+
+@end
+
+@implementation cloudit4iosTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
